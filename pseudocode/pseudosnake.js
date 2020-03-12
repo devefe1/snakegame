b89e6de11f26d6d87bdcae2018d741f0db59bac1
devefe1 <61803264+devefe1@users.noreply.github.com>
2020-03-12 11:55:55 -0500

    update

---
+++ pseudocode/pseudosnake.js
@@ -0,0 +1,49 @@
+// Snake Game
+
+// Player 1 = 1 live
+// Computer = 1 live
+
+// I create window
+// Create window
+// Load graphics and snake in different directions
+
+// Initialize variables
+//     create a 2d array representing the dimension or field
+//     create first head
+//     create first tail
+
+//     create computer head
+//     create computer tail
+//     create food
+
+// Reset game variables
+
+// My Loop
+//     if the arrow keys are pressed then
+//         Set snakes direction accordingly
+
+//     move snake's body according to head's direction and update the path in playing dimension array
+   
+
+    // if head pos = food pos then
+    //     add coins then, 
+    //     re-position food randomly
+
+//     draw food
+//     loop through 2d array drawing snake body where the snake has been
+//     draw snake head and tail according to their position
+//     draw current coins available
+
+//     // Collision detection
+//     if player 1 hits dimension at head or computer snake at head Then
+//         kill player
+//         or ask to respawn if amount of coins available correspond
+//         or restart game
+//         YOU LOSE !
+//         refresh variables
+//     if player 2 computer hits dimension at head or player 1 snake at head Then
+//     kill computer
+//     YOU Window
+//     restart game
+//     refresh variables
+// end main loop
